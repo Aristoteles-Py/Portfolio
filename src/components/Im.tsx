60e565bc88971185c0eadb15538cf274ff799b8f
Aristoteles-Py <arystotelys@gmail.com>
2023-01-02 22:52:43 -0300

    abaut my responsivo finalizado!

--- src/components/Im.tsx
+++ src/components/Im.tsx
@@ -5,14 +5,14 @@ import assinatura from '../assets/assinatura.svg';
 function Im() {
     return(
         <div className="w-fll mt-5 max-[650px]:p-4">
-            <div className="flex gap-7 justify-center  max-[650px]:h-[400px] max-[650px]:w-[400px]">
+            <div className="flex gap-7 justify-center  max-[650px]:h-[400px] max-[650px]:w-[400px] ">
 
-                <img className="h-[400px] w-[400px] rounded max-[650px]:absolute max-[650px]:w-[350px] max-[650px]:h-[350px]" 
+                <img className="h-[400px] w-[400px] rounded-2xl max-[650px]:absolute max-[650px]:w-[350px] max-[650px]:h-[350px] " 
                 src={myPhoto} 
                 alt=""
                 />
 
-                <div className="flex flex-col w-[400px] max-[650px]:relative max-[650px]:w-[350px] max-[650px]:h-[350px] max-[650px]:justify-end max-[650px]:text-right">
+                <div className="flex flex-col w-[400px] max-[650px]:relative max-[650px]:w-[350px] max-[650px]:h-[350px] max-[650px]:justify-end max-[650px]:text-right max-[650px]:overflow-hidden max-[650px]:rounded-2xl p-2">
 
                     <span className="max-[650px]:relative z-10">
                         Nice to meet you,
@@ -37,16 +37,16 @@ function Im() {
                     <img className="max-[650px]:hidden" src={assinatura} alt="" />
 
                     
-                    <div className='min-[650px]:hidden w-[500px] h-[280px] bg-white bg-opacity-50 absolute origin-center -rotate-[21deg] z-0 -mb-28'>
+                    <div className='min-[650px]:hidden w-[500px] h-[270px] bg-white bg-opacity-20 absolute origin-center -rotate-[18deg] z-0 -mb-28 backdrop-blur-sm'>
                     </div>
                 </div>
             </div>
 
-            <div className="w-full mt-4 pl-60 max-[650px]:pl-0">
+            <div className="w-full pl-60 max-[650px]:pl-0 max-[650px]:-mt-5 max-[650px]:justify-center max-[650px]:text-center">
                 <strong className="">
                     Minhas experiências inclui, mas não se limita:
                 </strong>
-                <ul className="flex flex-wrap gap-3 mt-1" >
+                <ul className="flex flex-wrap gap-3 mt-1 max-[650px]:justify-center" >
                     <li className="border-2 border-gold-100 px-1 rounded text-sm cursor-pointer">HTML</li>
                     <li className="border-2 border-gold-100 px-1 rounded text-sm cursor-pointer">CSS</li>
                     <li className="border-2 border-gold-100 px-1 rounded text-sm cursor-pointer">JavaScript</li>
